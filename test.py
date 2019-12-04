0055fb987c5e3e203b867244ca35fbf66a969b04
Futao <futao.gao@qq.com>
2019-12-04 20:00:50 +0800

    修正了人员信息类及列表排序方法

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ class Infos():
         self._ji_gou = None
         self._name = None
         self._ru_si_shi_jian = None
-        self._che_bao_fei = None
-        self._cai_bao_fei = None
-        self._ren_bao_fei = None
+        self._che = None
+        self._cai = None
+        self._ren = None
 
     @property
     def zhong_zhi(self):
@@ -45,54 +45,76 @@ class Infos():
     def ru_si_shi_jian(self, str):
         self._ru_si_shi_jian = str
 
-    @property
-    def che_bao_fei(self):
-        return self._che_bao_fei
-
-    @che_bao_fei.setter
-    def che_bao_fei(self, value):
-        self._che_bao_fei = value
-
-    @property
-    def cai_bao_fei(self):
-        return self._cai_bao_fei
-
-    @cai_bao_fei.setter
-    def cai_bao_fei(self, value):
-        self._cai_bao_fei = value
+    def set_che(self, nian_fen=None, yue_fen=None):
+        if nian_fen is not None:
+            self._che = self.bao_fei('车险', f"{nian_fen}")
+        elif yue_fen is not None:
+            self._che = self.bao_fei('车险', f"{nian_fen}", f"{yue_fen}")
+        else:
+            self._che = self.bao_fei('车险')
 
     @property
-    def ren_bao_fei(self):
-        return self._ren_bao_fei
+    def che(self):
+        return self._che
 
-    @ren_bao_fei.setter
-    def ren_bao_fei(self, value):
-        self._ren_bao_fei = value
+    def set_cai(self, nian_fen=None, yue_fen=None):
+        if nian_fen is not None:
+            self._cai = self.bao_fei('财产险', f"{nian_fen}")
+        elif yue_fen is not None:
+            self._cai = self.bao_fei('财产险', f"{nian_fen}", f"{yue_fen}")
+        else:
+            self._cai = self.bao_fei('财产险')
 
-    def __repr__(self):
-        return repr((self.zhong_zhi,
-                     self.ji_gou,
-                     self.name,
-                     self.ru_si_shi_jian,
-                     self.che_bao_fei,
-                     self.cai_bao_fei,
-                     self.ren_bao_fei))
+    @property
+    def cai(self):
+        return self._cai
 
-def bao_fei(name, zhong_zhi, xian_zhong):
-    str_sql = f"SELECT SUM([签单保费/批改保费]) \
-                FROM 销售人员业务跟踪表 \
-                WHERE 业务员 like '%{name}%' \
-                AND 中心支公司 like '%{zhong_zhi}' \
-                AND [车险/财产险/人身险] = '{xian_zhong}'"
+    def set_ren(self, nian_fen=None, yue_fen=None):
+        if nian_fen is not None:
+            self._ren = self.bao_fei('人身险', f"{nian_fen}")
+        elif yue_fen is not None:
+            self._ren = self.bao_fei('人身险', f"{nian_fen}", f"{yue_fen}")
+        else:
+            self._ren = self.bao_fei('人身险')
 
-    cur.execute(str_sql)
-    value = cur.fetchone()[0]
-    if value is None:
-        value = 0.0
-    else:
-        value /= 10000
+    @property
+    def ren(self):
+        return self._ren
 
-    return value
+    @property
+    def fei_che(self):
+        return self.cai + self.ren
+
+    # def __repr__(self):
+    #     return repr((self.zhong_zhi,
+    #                  self.ji_gou,
+    #                  self.name,
+    #                  self.ru_si_shi_jian,
+    #                  self.che_bao_fei,
+    #                  self.cai_bao_fei,
+    #                  self.ren_bao_fei))
+
+    def bao_fei(self, xian_zhong, nian_fen=None, yue_fen=None):
+        str_sql = f"SELECT SUM([签单保费/批改保费]) \
+                    FROM 销售人员业务跟踪表 \
+                    WHERE 业务员 like '%{self.name}%' \
+                    AND 中心支公司 like '%{self.zhong_zhi}' \
+                    AND [车险/财产险/人身险] = '{xian_zhong}'"
+
+        if nian_fen is not None:
+            str_sql += f" AND 年份 = '{nian_fen}'"
+
+        if yue_fen is not None:
+            str_sql += f" AND 月份 = '{yue_fen}'"
+
+        cur.execute(str_sql)
+        value = cur.fetchone()[0]
+        if value is None:
+            value = 0.0
+        else:
+            value /= 10000
+
+        return value
 
 
 conn = sqlite3.connect('data.db')
@@ -105,24 +127,18 @@ cur.execute(str_sql)
 rens = []
 
 for values in cur.fetchall():
+    info = Infos()
+    info.zhong_zhi = values[0][7:]
+    info.ji_gou = values[1][11:]
+    info.name = values[2][10:]
+    info.ru_si_shi_jian = values[3][:10]
+    info.set_cai()
+    info.set_che()
+    info.set_ren()
 
-    zhong_zhi = values[0][7:]
-    ji_gou = values[1][11:]
-    name = values[2][10:]
-    ru_si_shi_jian = values[3][:10]
-    che_bao_fei = bao_fei(name, zhong_zhi, '车险')
-    cai_bao_fei = bao_fei(name, zhong_zhi, '财产险')
-    ren_bao_fei = bao_fei(name, zhong_zhi, '人身险')
+    rens.append(info)
 
-    rens.append((zhong_zhi,
-                 ji_gou,
-                 name,
-                 ru_si_shi_jian,
-                 che_bao_fei,
-                 cai_bao_fei,
-                 ren_bao_fei))
-
-rens_sort = sorted(rens, key=lambda ren: ren[4], reverse = True)
+rens_sort = sorted(rens, key=lambda ren: ren.che, reverse=True)
 
 for ren in rens_sort:
-    print(ren)
+    print(ren.name, ren.che)
